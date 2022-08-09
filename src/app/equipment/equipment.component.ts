bbbcb7a4fda0cafa92595bbc883a3a61800bfc89
Linda Ryan <lwryan6@gmail.com>
2022-08-09 18:34:57 -0500

    Completed Part 1 of Chptr 31.7 Angular Studio 3.

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -27,5 +27,30 @@ export class EquipmentComponent implements OnInit {
    ngOnInit() { }
 
    // Code your addItem function here:
-   
+
+   // We wrote the function below in Studio class. It requires the result to be boolean but works w/o the : boolean
+  //  addItem(equipment: object) : boolean {
+  //   this.cargoHold.push(equipment);
+  //   this.cargoMass += equipment.mass; 
+  //   // Line below is what the solution code used instead of equipment.mass above b/c technically no mass property on the equipment object, but both work.
+  //   this.cargoMass += equipment['mass'];
+  //   // If/else conditional returns boolean, but can be inferred by the return line of the solution version at the bottom.
+  //   if (this.cargoMass >= 1800 || this.cargoMass <= 2200) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  //  }
+
+
+  
+   // This is the solution code method for the addItem(). Solution used item['mass'] which is better than item.mass, even though the latter DOES technically work.
+
+   addItem(item: object) : boolean {
+    this.cargoHold.push(item);
+    // Could use item.mass below to work, but NOT technically correct since mass property does not exist on the item object.
+    this.cargoMass += item['mass']; 
+    return this.maximumAllowedMass - this.cargoMass <= 200;
+   }
+
 }
